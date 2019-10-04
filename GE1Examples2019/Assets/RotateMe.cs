f4925f6c2240dff12ef13d52c0021be070bfaaf8
Adam Ryan <C16302271@mydit.ie>
2019-10-04 17:39:05 +0100

    lab3 stuff

---
+++ GE1Examples2019/Assets/RotateMe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xSpeed = 180f;
+        ySpeed = 180f;
+        zSpeed = 180f;
+        this.transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+    }
+}
